0c64b14a50ee97555df40d9fcca4a7859dd5117e
Rolex Brown <ranjanghimire@gmail.com>
2016-12-14 22:19:47 -0600

    Created UserInfo page

---
+++ src/pages/UserInfo/userinfo.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+import { NavController, NavParams } from 'ionic-angular';
+
+@Component({
+  selector: 'page-userinfo',
+  templateUrl: 'userinfo.html'
+})
+
+export class UserInfo{
+
+}
