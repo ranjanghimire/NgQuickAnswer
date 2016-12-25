a3d3f9c728e1622825211ccba1df828eb62afadf
Rolex Brown <ranjanghimire@gmail.com>
2016-12-24 21:32:35 -0600

    New Answer submission completed.

--- src/pages/answers-to-the-question/answers-to-the-question.ts
+++ src/pages/answers-to-the-question/answers-to-the-question.ts
@@ -1,7 +1,11 @@
 import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
-
-import { Question } from '../../models/app.question'
+import { Configuration } from '../../app/app.constants';
+import { Question } from '../../models/app.question';
+import { Answer } from '../../models/app.answer';
+import { AppUser } from '../../models/app.user';
+import {Author} from '../../models/app.author';
+import { AnswerService } from '../../shared/app.answerservice';
 
 @Component({
   selector: 'page-answers-to-the-question',
@@ -10,11 +14,42 @@ import { Question } from '../../models/app.question'
 export class AnswersToTheQuestionPage {
 
   private myQuestion : Question;
+  private myUserData : AppUser;
+  private myAnswer : Answer = new Answer();
+  private newAuthor : Author;
+  private retQuestion: Question;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(private _answerService: AnswerService, public navCtrl: NavController, public navParams: NavParams, private _conf : Configuration) {
     
     this.myQuestion = this.navParams.get("question");
-    
+    this.myUserData = _conf.myUser;
+  }
+
+  logAnswer(): void{
+    this.newAuthor  = new Author();
+    this.newAuthor.appUserId = this.myUserData.id;
+    this.newAuthor.appUserName = this.myUserData.userName;
+
+    this.myAnswer.author = this.newAuthor;
+
+    this.myAnswer.weight = 1;
+
+    this.postAnswer(this.myAnswer, this.myQuestion.id);
+  }
+
+  private postAnswer(answer: Answer, id: string): void{
+    this._answerService
+            .postAnswer(id, answer)
+            .subscribe((data:Question) => this.retQuestion = data,
+                error => console.log(error),
+                () => this.afterSubmit(answer));
+  }
+
+  afterSubmit(answer: Answer): void{
+      //Add the answer to the current list
+      //Show a toast
+      this.myQuestion.answers.push(answer);
+      console.log('Answer submitted');
   }
 
   ionViewDidLoad() {
