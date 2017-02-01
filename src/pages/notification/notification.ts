0e01c7ded5443a7206ed036a21a42f293dc49c59
Rolex Brown <ranjanghimire@gmail.com>
2017-01-31 19:07:22 -0600

    Timezone bug fixed in notification

--- src/pages/notification/notification.ts
+++ src/pages/notification/notification.ts
@@ -36,4 +36,19 @@ export class NotificationPage {
     this.navCtrl.push( AnswersToTheQuestionPage, { question : question });
   }
 
+  getTheTime(strDate: string){
+    return this.convertUTCDateToLocalDate(new Date(strDate));
+  }
+
+  convertUTCDateToLocalDate(date) {
+      var newDate = new Date(date.getTime()+date.getTimezoneOffset()*60*1000);
+
+      var offset = date.getTimezoneOffset() / 60;
+      var hours = date.getHours();
+
+      newDate.setHours(hours - offset);
+
+      return newDate;   
+  }
+
 }
